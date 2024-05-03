7d26e50cfc7f62bc0c669d56700572da6e2def9d
Smr021 <161032439+Smr021@users.noreply.github.com>
2024-05-03 12:24:14 +0000

    1. AulaPy

---
+++ 1. Tipos de dados/1. Olá Teste.py
@@ -0,0 +1,7 @@
+# Comentario de uma linha.
+
+'''
+Isso é um comentario de varias linhas.
+sasmdkas
+'''
+print("Digite um numero: ");
---
+++ 1. Tipos de dados/2. Tipagem dinamica.py
@@ -0,0 +1,17 @@
+#Tipagem Dinâmica.
+
+nome = 'Marta';
+idade = 20;
+peso = 60.500;
+
+#Exibindo dados.
+#Simples.
+print(nome);
+print(idade);
+print(peso);
+
+#Formatado.
+print(f'Nome : {nome}');
+print(f'Idade : {idade}');
+print(f'Peso : {peso}');
+
